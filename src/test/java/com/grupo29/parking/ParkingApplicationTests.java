ba10e446cc528afa2b516da204dc201e16f88392
Marcelo <marcelo.leite@mercadolivre.com>
2023-10-29 21:55:04 -0300

    [feat] initial config the project

---
+++ src/test/java/com/grupo29/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo29.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
